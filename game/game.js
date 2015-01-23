c0c50f5836c2697329d854544e9b14362857e0e2
rémi <remirobert33530@gmail.com>
2015-01-23 23:19:58 +0100

    fix background to the camera

--- game/game.js
+++ game/game.js
@@ -1,4 +1,5 @@
 var preloadRessource = function(game) {
   game.load.image('player','ressources/t.png');
   game.load.image('background','ressources/background.png');
+  game.load.image('background','ressources/frog1.png');
 }
--- game/main.js
+++ game/main.js
@@ -3,6 +3,8 @@ var initWindow = function() {
     var game = new Phaser.Game(1920, 1080, Phaser.CANVAS, '', { preload: preload, create: create, render:render, update:update});
     var player;
     var cursors;
+    var background1;
+    var background2;
 
     function preload () {
       preloadRessource(game);
@@ -12,7 +14,7 @@ var initWindow = function() {
       var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
       logo.anchor.setTo(0.5, 0.5);
 
-      game.add.tileSprite(0, 0, 10000, 1080, 'background');
+      background1 = game.add.sprite(0, 0, 'background');
       game.world.setBounds(0, 0, 10000, 1080);
       game.physics.startSystem(Phaser.Physics.P2JS);
       //game.physics.p2.gravity .y = 1500;
@@ -20,6 +22,7 @@ var initWindow = function() {
       game.physics.p2.enable(player);
 
       cursors = game.input.keyboard.createCursorKeys();
+      background1.fixedToCamera = true;
       game.camera.follow(player);
 
       game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
