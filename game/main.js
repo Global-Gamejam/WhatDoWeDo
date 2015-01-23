d33bba5e2111e887580eaa114f4d20acd8a7359b
rémi <remirobert33530@gmail.com>
2015-01-23 21:34:01 +0100

    add background and change size screen

--- game/main.js
+++ game/main.js
@@ -1,9 +1,6 @@
 var initWindow = function() {
   window.onload = function() {
-
-
-
-    var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, render:render, update:update});
+    var game = new Phaser.Game(1920, 1080, Phaser.CANVAS, '', { preload: preload, create: create, render:render, update:update});
     var player;
     var cursors;
 
@@ -15,13 +12,16 @@ var initWindow = function() {
       var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
       logo.anchor.setTo(0.5, 0.5);
 
-      game.world.setBounds(0, 0, 1920, 1920);
+      game.add.tileSprite(0, 0, 1920, 1920, 'background');
+      game.world.setBounds(0, 0, 1920 * 2, 600);
       game.physics.startSystem(Phaser.Physics.P2JS);
+      //game.physics.p2.gravity .y = 1500;
       player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
       game.physics.p2.enable(player);
       cursors = game.input.keyboard.createCursorKeys();
       game.camera.follow(player);
-      game.camera.deadzone = new Phaser.Rectangle(100, 100, 600, 400);
+      game.camera.deadzone = new Phaser.Rectangle(100, 100, 400, 400);
+      //game.stage.scale.startFullScreen();
     }
 
     function update() {
@@ -45,10 +45,8 @@ var initWindow = function() {
 
       game.context.fillStyle = 'rgba(255,0,0,0.6)';
       game.context.fillRect(zone.x, zone.y, zone.width, zone.height);
-
       game.debug.cameraInfo(game.camera, 32, 32);
       game.debug.spriteCoords(player, 32, 500);
-
     }
   };
 }
